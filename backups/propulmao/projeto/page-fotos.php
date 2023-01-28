ffdc76decec05daf569571e6547277f9475b7ba6
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-01-28 18:15:15 -0300

    fix

--- backups/propulmao/projeto/page-fotos.php
+++ backups/propulmao/projeto/page-fotos.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * Template Name: Fotos
@@ -12,70 +13,76 @@ get_header();
 <div>
 	Fotos
 	<section class="container  page-fotos">
+		<?php
+		// Check rows exists.
+		if (have_rows('galerias')) :
+			echo '<ul class="list-articles">';
+			// Loop through rows.
+			while (have_rows('galerias')) :
+				the_row();
+		?>
+		<li class="item-article">
+			<article>
+				<h2>
+					<?php echo get_sub_field('gallery_name') ?>
+				</h2>
+
+				<p>
+					<?php echo get_sub_field('gallery_text') ?>
+				</p>
+
+
+				<?php
+				$lista = get_sub_field('gallery_imagens');
+				if(!empty($lista)){
+						echo '<div class="center">';
+						foreach ($lista as $item) {
+								$img_src = htmlspecialchars($item['gallery_imagen']);
+								echo '<li><img src="' . $img_src . '"></li>';
+						}
+						echo '</div>';
+				}
+		?>
+		<li>
+
+		</li>
+		</article>
+		</li>
+		<?php
+			// End loop.
+			endwhile;
+			echo '</ul>';
+			?>
+		<?php
+		// No value.
+		else :
+		// No rows found
+		endif;
+		?>
+
 		<ul>
 			<li class="item-article">
-				<article
-				">
+				<article ">
 				<h2>
 					Galeria 1
 				</h2>
 				<p>Galeria de imagens do evento X</p>
+</div>
 
-				<div class="swiper">
-					<!-- Additional required wrapper -->
-					<div class="swiper-wrapper">
-						<!-- Slides -->
-						<div class="swiper-slide">Slide 1</div>
-						<div class="swiper-slide">Slide 2</div>
-						<div class="swiper-slide">Slide 3</div>
-						...
-					</div>
-					<!-- If we need pagination -->
-					<div class="swiper-pagination"></div>
 
-					<!-- If we need navigation buttons -->
-					<div class="swiper-button-prev"></div>
-					<div class="swiper-button-next"></div>
 
-					<!-- If we need scrollbar -->
-					<div class="swiper-scrollbar"></div>
-				</div>
-
-				<ul class="list-images">
-					<li>
-						<div class="container-image">
-							<div></div>
-						</div>
-					</li>
-
-					<li>
-						<div class="container-image">
-							<div></div>
-						</div>
-					</li>
-
-					<li>
-						<div class="container-image">
-							<div></div>
-						</div>
-					</li>
-
-					<li>
-						<div class="container-image">
-							<div></div>
-						</div>
-					</li>
-
-					<li>
-						<div class="container-image">
-							<div></div>
-						</div>
-					</li>
-				</ul>
-				</article>
-			</li>
-		</ul>
-	</section>
+		<!-- <div class=" center">
+					<div>your content</div>
+					<div>your content</div>
+					<div>your content</div>
+					<div>your content</div>
+					<div>your content</div>
+					<div>your content</div>
+					<div>your content</div>
+					<div>your content</div>
+					<div>your content</div>
+</div> -->
+</section>
 </div>
 
 <?php get_footer(); ?>
