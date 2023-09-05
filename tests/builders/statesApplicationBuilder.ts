e6af5f331d72bc4cbd04672114fb70903bae223f
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-05 07:29:08 -0300

    fix: correct imports for StatesApplication and statesDefault

--- tests/builders/statesApplicationBuilder.ts
+++ tests/builders/statesApplicationBuilder.ts
@@ -1,7 +1,7 @@
 import {
-  type StatesApplication,
-  statesDefault
 } from '../../src/views/context/reducers/reducerApplication/reducerApplication.ts'
+import { type StatesApplication } from '../../src/views/context/reducers/reducerApplication/types.ts'
+import { statesDefault } from '../../src/views/context/reducers/reducerApplication/utils/generateDefaultStates'
 
 export class StatesApplicationBuilder {
   #state: StatesApplication
