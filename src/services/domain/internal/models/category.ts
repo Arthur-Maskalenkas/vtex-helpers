2aba0b4aa202c21fd8281990d6aaa4681bb07a78
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-14 22:00:24 -0300

    feat: transform domain model in class instead interface

--- src/services/domain/internal/models/category.ts
+++ src/services/domain/internal/models/category.ts
@@ -1,6 +1,14 @@
-export interface InternalModelCategory {
-  id: number
-  name: string
-  url: string
-  children: InternalModelCategory[]
+export class InternalModelCategory {
+  public readonly id: number
+  public readonly name: string
+  public readonly url: string
+  public readonly children: InternalModelCategory[]
+  constructor (attrs: InternalModelCategory) {
+    const { id, name, url, children } = attrs
+
+    this.id = id
+    this.name = name
+    this.url = url
+    this.children = children
+  }
 }
