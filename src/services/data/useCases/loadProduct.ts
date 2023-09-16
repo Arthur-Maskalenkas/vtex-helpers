b6c00224d9fe5fad6e4b5237ce27f9195c739131
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-16 14:04:15 -0300

    feat(data): implements use case load product

---
+++ src/services/data/useCases/loadProduct.ts
@@ -0,0 +1,19 @@
+import { type IUseCaseLoadProduct } from '../../domain/useCases/loadProduct.ts'
+import { type ProtocolRepositoryLoadProduct } from '../protocols/repositoryLoadProduct.ts'
+import { type ProtocolMapModelProduct } from '../protocols/mapModelProduct.ts'
+
+export class UsecaseLoadProduct implements IUseCaseLoadProduct {
+  constructor (
+    private readonly repositoryProduct: ProtocolRepositoryLoadProduct,
+    private readonly mapModelProduct: ProtocolMapModelProduct
+  ) {}
+
+  async load (params: IUseCaseLoadProduct.Params): IUseCaseLoadProduct.Result {
+    const product = await this.repositoryProduct.load(params)
+    const internalProduct = this.mapModelProduct.map(product)
+
+    return internalProduct
+  }
+}
+
+// feat: add a class UsecaseLoadProduct with implementation  of IUseCaseLoadProduct
