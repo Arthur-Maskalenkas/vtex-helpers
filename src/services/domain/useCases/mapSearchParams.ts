8b806aa278d284021919aa8d5d22a06cc1e68275
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-17 09:29:08 -0300

    feat(domain): add mapSearchParams use case

---
+++ src/services/domain/useCases/mapSearchParams.ts
@@ -0,0 +1,9 @@
+export interface IUsecaseMapSearchParams {
+  map: (params: IUsecaseMapSearchParams.Params) => IUsecaseMapSearchParams.Result
+}
+
+export namespace IUsecaseMapSearchParams {
+  export type Result = string | null
+
+  export type Params = string
+}
