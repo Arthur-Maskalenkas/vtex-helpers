12677e85146c94ff013833ec62869e0f96817edd
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-17 09:29:42 -0300

    feat(data): update mapSearchParams protocol

--- src/services/data/protocols/mapSearchParams.ts
+++ src/services/data/protocols/mapSearchParams.ts
@@ -1,9 +1,5 @@
+import { type IUsecaseMapSearchParams } from '../../domain/useCases/mapSearchParams.ts'
+
 export interface ProtocolMapSearchparams {
-  map: (params: ProtocolMapSearchparams.Params) => ProtocolMapSearchparams.Result
-}
-
-export namespace ProtocolMapSearchparams {
-  export type Result = string | null
-
-  export type Params = string
+  map: (params: IUsecaseMapSearchParams.Params) => IUsecaseMapSearchParams.Result
 }
