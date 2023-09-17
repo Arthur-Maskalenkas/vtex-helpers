4342871239740efd762920ed3df3af30592d1cb2
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-17 09:30:23 -0300

    feat(data): add implementation of use case mapSearchParams

---
+++ src/services/data/useCases/mapSearchParams.ts
@@ -0,0 +1,13 @@
+import { type IUsecaseMapSearchParams } from '../../domain/useCases/mapSearchParams.ts'
+import { type ProtocolMapSearchparams } from '../protocols/mapSearchParams.ts'
+
+export class UsecaseMapSearchParams implements IUsecaseMapSearchParams {
+  constructor (
+    private readonly protocolMapSearchParams: ProtocolMapSearchparams
+  ) {
+  }
+
+  map (params: IUsecaseMapSearchParams.Params): IUsecaseMapSearchParams.Result {
+    return this.protocolMapSearchParams.map(params)
+  }
+}
