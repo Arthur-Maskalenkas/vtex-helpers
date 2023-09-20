8e5b70f7e5beb9396a36058ebca8c1f1628f1bcf
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-20 00:56:07 -0300

    refactor(view): create specific suite tests from 'mapParams' fn

--- tests/views/screens/commons/form/form.spec.tsx
+++ tests/views/screens/commons/form/form.spec.tsx
@@ -3,52 +3,55 @@ import { render, userEvent } from '../../../../utils/test-utils.tsx'
 import { expect } from 'vitest'
 import { ListOptions } from '../../../../../src/views/molecules/listOptions.tsx'
 import { BuilderForm } from './builders.tsx'
+import { mapParams } from '../../../../../src/views/screens/commons/form/utils.ts'
 
 describe(Form.name, () => {
   beforeEach(() => {
     vi.resetAllMocks()
   })
 
-  it('should call clearSubmit with inputs', () => {
-    const { fnSpy } = BuilderForm.a().appendInput('paramCollection', 'value.0').build()
+  describe(mapParams.name, () => {
+    it('should call clearSubmit with inputs', () => {
+      const { fnSpy } = BuilderForm.a().appendInput('paramCollection', 'value.0').build()
 
-    expect(fnSpy).toHaveBeenCalledWith('paramCollection=value.0')
-  })
+      expect(fnSpy).toHaveBeenCalledWith('paramCollection=value.0')
+    })
 
-  it('should receive multiple inputs', () => {
-    const { fnSpy } = BuilderForm
-      .a()
-      .appendInput('paramCollection', 'collection')
-      .appendInput('paramCategory', 'category')
-      .appendInput('paramSpecification', 'specificationId=specificationValue')
-      .build()
+    it('should receive multiple inputs', () => {
+      const { fnSpy } = BuilderForm
+        .a()
+        .appendInput('paramCollection', 'collection')
+        .appendInput('paramCategory', 'category')
+        .appendInput('paramSpecification', 'specificationId=specificationValue')
+        .build()
 
-    expect(fnSpy).toHaveBeenCalledWith('paramCollection=collection,paramCategory=category,paramSpecification=specificationId=specificationValue')
-  })
+      expect(fnSpy).toHaveBeenCalledWith('paramCollection=collection,paramCategory=category,paramSpecification=specificationId=specificationValue')
+    })
 
-  it('should join params by character "=" if input have "data-with-value" attribute', () => {
-    const { fnSpy } = BuilderForm
-      .a()
-      .appendInput('paramSpecification', 'specificationId=specificationValue', 'data-with-value')
-      .build()
+    it('should join params by character "=" if input have "data-with-value" attribute', () => {
+      const { fnSpy } = BuilderForm
+        .a()
+        .appendInput('paramSpecification', 'specificationId=specificationValue', 'data-with-value')
+        .build()
 
-    expect(fnSpy).toHaveBeenCalledWith('paramSpecification=specificationId=specificationValue')
-  })
+      expect(fnSpy).toHaveBeenCalledWith('paramSpecification=specificationId=specificationValue')
+    })
 
-  it('should not call when not have inputs', () => {
-    const { fnSpy } = BuilderForm
-      .a()
-      .build()
+    it('should not call when not have inputs', () => {
+      const { fnSpy } = BuilderForm
+        .a()
+        .build()
 
-    expect(fnSpy).not.toHaveBeenCalled()
-  })
+      expect(fnSpy).not.toHaveBeenCalled()
+    })
 
-  it('should not call fn if input have "data-with-value" attribute but not have value', () => {
-    const { fnSpy } = BuilderForm
-      .a()
-      .appendInput('paramSpecification', '', 'data-with-value')
-      .build()
+    it('should not call fn if input have "data-with-value" attribute but not have value', () => {
+      const { fnSpy } = BuilderForm
+        .a()
+        .appendInput('paramSpecification', '', 'data-with-value')
+        .build()
 
-    expect(fnSpy).not.toHaveBeenCalled()
+      expect(fnSpy).not.toHaveBeenCalled()
+    })
   })
 })
