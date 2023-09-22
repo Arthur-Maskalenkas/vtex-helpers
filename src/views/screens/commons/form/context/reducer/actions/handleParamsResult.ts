a11308b8f7adf1a761bfcb8d8ddc49160cb8a9be
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-22 01:05:24 -0300

    feat(view): Ensure the correctly state is returned from actionHandleParamsResult

---
+++ src/views/screens/commons/form/context/reducer/actions/handleParamsResult.ts
@@ -0,0 +1,19 @@
+import { ReducerFormSearchParams } from "../reducer.ts";
+
+
+
+export type ActionHandleParamsResult = {
+		type: 'ACTION_HANDLE_PARAMS_RESULT'
+		payload: {
+				data: {
+						params: string
+				}
+		}
+}
+
+export const actionHandleParamsResult = ({ states, action }: ReducerFormSearchParams.Params<ActionHandleParamsResult>): ReducerFormSearchParams.States => {
+		return {
+				...states,
+				urlGenerated: action.payload.data.params
+		}
+}
