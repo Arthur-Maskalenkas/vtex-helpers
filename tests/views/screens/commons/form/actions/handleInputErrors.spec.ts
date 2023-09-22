33d28b0e8386845f56ce5556ec329ee6d014baf9
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-22 00:30:51 -0300

    test(view): ensure ActionHandleInputErrors returns a correctly states

---
+++ tests/views/screens/commons/form/actions/handleInputErrors.spec.ts
@@ -0,0 +1,41 @@
+import {
+		ActionHandleInputErrors, actionHandleInputErrors
+} from "../../../../../../src/views/screens/commons/form/context/reducer/actions/handleInputErrors.ts";
+import { BuilderReducerFormSearchParams } from "./builders.ts";
+import { ReducerFormSearchParams } from "../../../../../../src/views/screens/commons/form/context/reducer/reducer.ts";
+import { expect } from 'vitest'
+
+
+
+const useSut = (states: ReducerFormSearchParams.States, payload: Map<string, string>): ReducerFormSearchParams.States => {
+		const action: ActionHandleInputErrors = {
+				type: 'ACTION_HANDLE_INPUT_ERRORS',
+				payload: {
+						data: {
+								inputs: payload
+						}
+				}
+		}
+
+		return actionHandleInputErrors({ states, action })
+}
+
+describe(actionHandleInputErrors.name, () => {
+		it('Should return a correctly states', () => {
+				const states = BuilderReducerFormSearchParams.a().build()
+
+				const payload = new Map([ [ 'name', 'error' ] ])
+
+				const result = useSut(states, payload)
+
+				const expectedState = {
+						...states,
+						hasErrors: true,
+						inputsWithErrors: payload
+				}
+
+				expect(result).toStrictEqual(expectedState)
+		})
+
+
+})
