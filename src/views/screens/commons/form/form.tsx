1e90b7a530d7004a48c2af3694ee12b98c863010
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-23 19:32:53 -0300

    feat(view): ensure Form handleSubmit calls dispatch with action ACTION_HANDLE_INPUT_ERRORS

--- src/views/screens/commons/form/form.tsx
+++ src/views/screens/commons/form/form.tsx
@@ -1,33 +1,37 @@
 import { Button as ButtonCommon } from '../button/button.tsx'
 import './styles.scss'
 import { mapParams } from './utils.ts'
-import { FormProvider } from "./context";
+import { FormProvider, useFormContext } from "./context";
 
 
 
 export const Button = ({ children }: React.PropsWithChildren) => {
 		return (
-					<ButtonCommon type={'submit'}>
-							{children}
-					</ButtonCommon>
+				<ButtonCommon type={'submit'}>
+						{children}
+				</ButtonCommon>
 		)
 }
 
 export const Form = ({ children, handleSubmit }: React.PropsWithChildren<{
 		handleSubmit: (data: string | Map<any, any> | null) => void
 }>) => {
+		const { dispatch } = useFormContext()
+
 		const clearSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 				event.preventDefault()
 				const paramsMapped = mapParams(event)
+
+				dispatch({ type: 'ACTION_HANDLE_INPUT_ERRORS', payload: { data: { inputs: paramsMapped } } })
 				paramsMapped && handleSubmit(paramsMapped)
 		}
 
 		return (
-					<FormProvider>
-							<form role={'form'} onSubmit={clearSubmit} className={'component-form form common'}>
-									{children}
-							</form>
-					</FormProvider>
+				<FormProvider>
+						<form role={'form'} onSubmit={clearSubmit} className={'component-form form common'}>
+								{children}
+						</form>
+				</FormProvider>
 		)
 }
 Form.Button = Button
