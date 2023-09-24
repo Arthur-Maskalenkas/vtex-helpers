83978386bd5f3bb067cbb4ed02cb73caf5e79131
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-24 01:17:54 -0300

    feat(view): remove unused imports and classes from form builders

--- tests/views/screens/commons/form/builders.tsx
+++ tests/views/screens/commons/form/builders.tsx
@@ -1,10 +1,3 @@
-import { render, userEvent } from '../../../../utils/test-utils.tsx'
-import { Form } from '../../../../../src/views/screens/commons/form/form.tsx'
-import * as utilsModule from '../../../../../src/views/screens/commons/form/utils.ts'
-import { vi } from "vitest";
-
-
-
 export class BuilderReactFormEvent {
 		#elements: any[] = []
 
@@ -65,48 +58,5 @@ export class BuilderReactFormEvent {
 								elements: this.#elements
 						}
 				}
-		}
-}
-
-export class BuilderForm {
-		#inputs: any[] = []
-		#submitOnStart: boolean
-
-		private constructor(submitOnStart = true) {
-				this.#submitOnStart = submitOnStart
-		}
-
-		public static a(submitOnStart = true) {
-				return new BuilderForm(submitOnStart)
-		}
-
-		public appendInput(id?: string, value?: string, ...rest: any) {
-				this.#inputs.push(
-						<input {...rest} id={id} value={value}/>
-				)
-				return this
-		}
-
-		public mockAMapParams(value: string) {
-				vi.spyOn(utilsModule, utilsModule.mapParams.name as any)
-						.mockImplementation(() => value)
-				return this
-		}
-
-		public build() {
-				const fnSpy = vi.fn()
-				const renderedEl = render(
-						<Form handleSubmit={fnSpy}>
-								{this.#inputs}
-								<Form.Button>Submit</Form.Button>
-						</Form>
-				)
-
-				if (this.#submitOnStart) {
-						const renderedButton = renderedEl.getByRole('button')
-						userEvent.click(renderedButton)
-				}
-
-				return { sut: renderedEl.queryByRole('form'), renderedEl, fnSpy }
 		}
 }
