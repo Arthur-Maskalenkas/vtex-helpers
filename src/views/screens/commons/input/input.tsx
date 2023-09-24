c27f5882ae32769e9c5d6d5de1ae303b972444ee
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-24 00:51:15 -0300

    feat(view): Add subcomponent Description

--- src/views/screens/commons/input/input.tsx
+++ src/views/screens/commons/input/input.tsx
@@ -1,26 +1,35 @@
-import React, { type HTMLInputTypeAttribute, type InputHTMLAttributes } from 'react'
+import React, { type InputHTMLAttributes } from 'react'
 import './styles.scss'
 
 
 
 const Container = ({ children }: React.PropsWithChildren) => {
 		return (
-					<div className="component-input container common">
-							{children}
-					</div>
+				<div className="component-input container common">
+						{children}
+				</div>
 		)
 }
 const Label = ({ children, htmlFor }: React.PropsWithChildren<{ htmlFor: string }>) => {
 		return (
-					<label htmlFor={htmlFor} className="component-input label common">
-							{children}
-					</label>
+				<label htmlFor={htmlFor} className="component-input label common">
+						{children}
+				</label>
 		)
 }
 
-export const Input = ({ value, id, name, ...rest }: InputHTMLAttributes<HTMLInputTypeAttribute>) => {
-		return <Input {...rest} value={value} id={id} name={name} className="component-input input common"/>
+const Description = ({ children, ariaDescribedBy }: React.PropsWithChildren<{ ariaDescribedBy: string }>) => {
+		return (
+				<span id={ariaDescribedBy} className="component-input description common">
+						{children}
+				</span>
+		)
+}
+
+export const Input = ({ value, id, name, ...rest }: InputHTMLAttributes<HTMLInputElement>) => {
+		return <input  {...rest} type={"text"} value={value} id={id} name={name} className="component-input input common"/>
 }
 
 Input.Container = Container
 Input.Label = Label
+Input.Description = Description
