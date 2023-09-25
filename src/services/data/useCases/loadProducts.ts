9606c18df1d62e063de734266973e7c4fefd2a89
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-24 21:27:29 -0300

    feat(data): add UsecaseLoadProducts class and related imports

---
+++ src/services/data/useCases/loadProducts.ts
@@ -0,0 +1,20 @@
+import { type IUseCaseLoadProduct } from '../../domain/useCases/loadProduct.ts'
+import { type ProtocolMapModelProduct } from '../protocols/mapModelProduct.ts'
+import { IUseCaseLoadProducts } from "../../domain/useCases/loadProducts.ts";
+import { ProtocolRepositoryLoadProducts } from "../protocols/repositoryLoadProducts.ts";
+import { ProtocolMapSearchProductParams } from "../protocols/mapSearchProductParams.ts";
+
+
+
+export class UsecaseLoadProducts implements IUseCaseLoadProducts {
+		constructor(
+				private readonly repositoryProduct: ProtocolRepositoryLoadProducts,
+				private readonly mapModelProduct: ProtocolMapModelProduct,
+				private readonly mapParams: ProtocolMapSearchProductParams
+		) {
+		}
+
+		async load(params: IUseCaseLoadProduct.Params): IUseCaseLoadProducts.Result {
+				return params as any
+		}
+}
