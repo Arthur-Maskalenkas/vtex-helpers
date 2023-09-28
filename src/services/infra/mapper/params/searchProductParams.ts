94e8a90d140f44bcf0cb55a82eb2a5c270f3eab7
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-27 23:17:50 -0300

    feat(infra): create a MapperSearchProductParams class

---
+++ src/services/infra/mapper/params/searchProductParams.ts
@@ -0,0 +1,12 @@
+import { ProtocolMapSearchProductParams } from "../../../data/protocols/mapSearchProductParams.ts";
+import { IUsecaseMapSearchPageParams } from "../../../domain/useCases/mapSearchPageParams.ts";
+
+
+
+export class MapperSearchProductParams implements ProtocolMapSearchProductParams {
+		public map(params: IUsecaseMapSearchPageParams.Params): IUsecaseMapSearchPageParams.Result {
+				return null as any;
+		}
+
+
+}
