d70202aa02825701e1c898c4eec32b800684ebfd
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-27 23:24:50 -0300

    test(infra): ensure MapperSearchProductParams returns null if params is null

--- tests/services/infra/mapper/searchProductParams.spec.ts
+++ tests/services/infra/mapper/searchProductParams.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { UsecaseLoadProducts } from "../../../../src/services/data/useCases/loadProducts.ts";
 import { MapperSearchProductParams } from "../../../../src/services/infra/mapper/params/searchProductParams.ts";
 
@@ -13,6 +13,12 @@ describe(`${UsecaseLoadProducts.name} Tests Suite`, () => {
 
 
 		describe(`# ${MapperSearchProductParams.prototype.map.name}`, () => {
+
+				it('Should return null if	params is null', () => {
+						const result = sut.map(null as any)
+
+						expect(result).toBeNull()
+				})
 		})
 
 })
