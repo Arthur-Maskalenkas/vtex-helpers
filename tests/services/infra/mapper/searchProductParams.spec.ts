1eba2dab8a972ed94af7ac38803c0b66a36e4dc3
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-27 23:34:33 -0300

    test(infra): ensure MapperSearchProductParams returns a product id param mapped

--- tests/services/infra/mapper/searchProductParams.spec.ts
+++ tests/services/infra/mapper/searchProductParams.spec.ts
@@ -1,13 +1,17 @@
 import { beforeEach, describe, expect, it } from "vitest";
 import { UsecaseLoadProducts } from "../../../../src/services/data/useCases/loadProducts.ts";
 import { MapperSearchProductParams } from "../../../../src/services/infra/mapper/params/searchProductParams.ts";
+import { faker } from "@faker-js/faker";
 
 
 
 describe(`${UsecaseLoadProducts.name} Tests Suite`, () => {
 		let sut: MapperSearchProductParams
+		let id
 
 		beforeEach(() => {
+				id = faker.string.uuid()
+
 				sut = new MapperSearchProductParams()
 		})
 
@@ -18,6 +22,14 @@ describe(`${UsecaseLoadProducts.name} Tests Suite`, () => {
 						const result = sut.map(null as any)
 
 						expect(result).toBeNull()
+				})
+
+				it('Should return a product id param mapped', () => {
+						const result = sut.map(`paramProductId=${id}`)
+
+						const expected = `productId:${id}`
+
+						expect(result).toBe(expected)
 				})
 		})
 
