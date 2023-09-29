f9e5da792d3e6d4b2b60023b14f1167872c08147
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-29 15:30:11 -0300

    test(view): ensure Switch returns a default case

--- tests/views/common/switch.spec.tsx
+++ tests/views/common/switch.spec.tsx
@@ -45,12 +45,27 @@ describe(`${Switch.name} Tests Suite`, () => {
 						expect(getByText(caseContent2)).toBeInTheDocument()
 						expect(queryByText(caseContent3)).not.toBeInTheDocument()
 				})
-				it('Should return first case matched', () => {
-				})
-				it('Should return default case', () => {
-				})
 				it('Should return default case when cases	not match', () => {
+						const caseContent1 = 'case content 1'
+						const caseContent2 = 'case content 2'
+						const caseContent3 = 'case content 3'
+						const caseContent4 = 'case content 4'
+
+						const { getByText, queryByText } = render(
+								<Switch.Container>
+										<Switch.Case condition={false}>{caseContent1}</Switch.Case>
+										<Switch.Case condition={false}>{caseContent2}</Switch.Case>
+										<Switch.Case condition={false}>{caseContent3}</Switch.Case>
+										<Switch.DefaultCase>{caseContent4}</Switch.DefaultCase>
+								</Switch.Container>)
+
+						expect(queryByText(caseContent1)).not.toBeInTheDocument()
+						expect(queryByText(caseContent2)).not.toBeInTheDocument()
+						expect(queryByText(caseContent3)).not.toBeInTheDocument()
+
+						expect(getByText(caseContent4)).toBeInTheDocument()
 				})
+
 				it('Should return null when not have default	case and cases not match', () => {
 				})
 		})
