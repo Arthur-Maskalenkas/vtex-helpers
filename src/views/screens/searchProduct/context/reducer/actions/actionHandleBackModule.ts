b3d84977a7917a1383df2086b070e7f02fa995ea
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-29 14:22:39 -0300

    feat(view): ensure actionHandleBackModule returns to module form when current module is search

--- src/views/screens/searchProduct/context/reducer/actions/actionHandleBackModule.ts
+++ src/views/screens/searchProduct/context/reducer/actions/actionHandleBackModule.ts
@@ -10,8 +10,14 @@ export namespace ActionHandleBackModule {
 }
 
 export const actionHandleBackModule = ({ states, action }: ReducerSearchProduct.Params<ActionHandleBackModule.Dispatch>): ReducerSearchProduct.States => {
+		const strategies = new Map<ReducerSearchProduct.Modules, ReducerSearchProduct.Modules>(
+				[ [ 'product', 'search' ], [ 'search', 'form' ] ]
+		)
+
+		const currentModule = strategies.get(states.currentModule)
+
 		return {
 				...states,
-				currentModule: 'search',
+				currentModule: currentModule || 'form',
 		}
 }
