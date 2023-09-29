22bb03c790e147635bed439e926d0c968787434d
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-29 13:06:18 -0300

    feat(view): ensure actionHandleParamsResult replaces a currentModule and form.products attrs

---
+++ src/views/screens/searchProduct/context/reducer/actions/handleParamsResult.ts
@@ -0,0 +1,28 @@
+import { ReducerSearchProduct } from "../reducer.ts";
+import { Product } from "../../../../../../services/domain/internal/models/product.ts";
+
+
+
+export namespace ActionHandleParamsResult {
+
+		export type Payload = Product.Current[]
+
+		export type Action = {
+				type: 'ACTION_HANDLE_PARAMS_RESULT'
+				payload: {
+						data: Payload
+				}
+		}
+
+}
+
+export const actionHandleParamsResult = ({ states, action }: ReducerSearchProduct.Params<ActionHandleParamsResult.Action>): ReducerSearchProduct.States => {
+		return {
+				...states,
+				currentModule: 'search',
+				form: {
+						...states.form,
+						products: action.payload.data
+				}
+		}
+}
