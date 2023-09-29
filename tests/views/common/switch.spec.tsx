8997835dcd1021e867a683ba5f16377e91bf1912
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-29 15:27:26 -0300

    test(view): ensure switch return first case matched only

--- tests/views/common/switch.spec.tsx
+++ tests/views/common/switch.spec.tsx
@@ -5,7 +5,16 @@ import { faker } from "@faker-js/faker";
 
 
 
+const renderSut = (childrens: any) => {
+		const { getByText } = render(<Switch.Container>{childrens}</Switch.Container>)
+
+		return {
+				getByText
+		}
+}
+
 describe(`${Switch.name} Tests Suite`, () => {
+
 
 		describe(`# ${Switch.name}`, () => {
 				it('Should return case', () => {
@@ -20,7 +29,30 @@ describe(`${Switch.name} Tests Suite`, () => {
 						expect(getByText(caseContent)).toBeInTheDocument()
 				})
 
+				it('Should return first case only', () => {
+						const caseContent1 = 'case content 1'
+						const caseContent2 = 'case content 2'
+						const caseContent3 = 'case content 3'
 
+						const { getByText, queryByText } = render(
+								<Switch.Container>
+										<Switch.Case condition={false}>{caseContent1}</Switch.Case>
+										<Switch.Case condition={true}>{caseContent2}</Switch.Case>
+										<Switch.Case condition={true}>{caseContent3}</Switch.Case>
+								</Switch.Container>)
+
+						expect(queryByText(caseContent1)).not.toBeInTheDocument()
+						expect(getByText(caseContent2)).toBeInTheDocument()
+						expect(queryByText(caseContent3)).not.toBeInTheDocument()
+				})
+				it('Should return first case matched', () => {
+				})
+				it('Should return default case', () => {
+				})
+				it('Should return default case when cases	not match', () => {
+				})
+				it('Should return null when not have default	case and cases not match', () => {
+				})
 		})
 
 })
