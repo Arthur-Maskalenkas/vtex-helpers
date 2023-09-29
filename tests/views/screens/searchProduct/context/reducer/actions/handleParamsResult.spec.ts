1a0ec36075e52d7b2beda376a1b485051c769c9d
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-29 13:08:05 -0300

    test(view): ensure actionHandleParamsResult replaces a currentModule and form.products attrs

---
+++ tests/views/screens/searchProduct/context/reducer/actions/handleParamsResult.spec.ts
@@ -0,0 +1,65 @@
+import {
+		ActionHandleParamsResult, actionHandleParamsResult
+} from "../../../../../../../src/views/screens/searchProduct/context/reducer/actions/handleParamsResult.ts";
+import {
+		ReducerSearchProduct, ReducerSearchProductParamsDefaultValues
+} from "../../../../../../../src/views/screens/searchProduct/context/reducer/reducer.ts";
+import { faker } from "@faker-js/faker";
+import { beforeEach, describe, expect, it } from "vitest";
+
+
+
+const useSut = (states: ReducerSearchProduct.States, payload: ActionHandleParamsResult.Payload): ReducerSearchProduct.States => {
+		const action: ActionHandleParamsResult.Action = {
+				type: 'ACTION_HANDLE_PARAMS_RESULT',
+				payload: {
+						data: payload
+				}
+		}
+
+		return actionHandleParamsResult({ states, action })
+}
+
+const fakerObj = (quantityAttrs = 2): any => {
+		const obj = new Map<string, string>()
+
+		for (let i = 0; obj.size < quantityAttrs; i++) {
+				let key = faker.word.words(1)
+				let value = faker.random.word()
+
+				obj.set(key, value)
+		}
+
+		return Object.fromEntries(obj)
+}
+
+
+describe(`${actionHandleParamsResult.name} Tests Suite`, () => {
+
+		let defaultStates: ReducerSearchProduct.States
+
+		beforeEach(() => {
+				defaultStates = ReducerSearchProductParamsDefaultValues
+		})
+
+		it('Should update a form products and replace currentModule', () => {
+				const payload: ActionHandleParamsResult.Payload = [
+						fakerObj(3),
+						fakerObj(3),
+				]
+
+				const result = useSut(defaultStates, payload)
+
+				expect(result.currentModule).toBe('search')
+				expect(result.form.products).toEqual(payload)
+		})
+
+		it('Should replace existent products in states with new products', () => {
+		})
+
+		it('Should clear inputs with errors', () => {
+		})
+
+		it('Should clear current product', () => {
+		})
+})
