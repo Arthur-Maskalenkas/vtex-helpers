91b2612366fefadd133d953451199ab71e912e90
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-30 09:21:33 -0300

    feat(view): useApplicationReducerContext to form.tsx in searchProduct module

--- src/views/screens/searchProduct/modules/form/form.tsx
+++ src/views/screens/searchProduct/modules/form/form.tsx
@@ -1,14 +1,16 @@
 import { useSearchProductContext } from "../../context";
 import { handleSubmit } from "./utils/handleSubmit.ts";
+import { useApplicationReducerContext } from "../../../../application/context";
 
 
 
 export const ModuleForm = () => {
 		const { dispatch, states } = useSearchProductContext()
+		const { state: { appInternalServices: { product: { loadProducts: controller } } } } = useApplicationReducerContext()
 
 
 		return (
-				<form role={'form'} onSubmit={e => handleSubmit(e, dispatch)}>
+				<form role={'form'} onSubmit={e => handleSubmit(e, dispatch, controller as any)}>
 
 				</form>
 		)
