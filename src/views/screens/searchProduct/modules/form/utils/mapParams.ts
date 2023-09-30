f2181aa9959482a15505a861a64632776a47841d
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-30 18:01:30 -0300

    refactor(view): implements use formdata hook  instead of form event in mapParams

--- src/views/screens/searchProduct/modules/form/utils/mapParams.ts
+++ src/views/screens/searchProduct/modules/form/utils/mapParams.ts
@@ -1,42 +1,16 @@
-import { ReducerSearchProduct } from "../../../context/reducer/reducer.ts";
-
-
-
 export const errorMessages = {
 		specification: {
 				requiredValue: 'É necessario informar o valor da especificação. O formato para informar o valor é: "id=valor". Exemplo: Cor=Azul',
 		}
 }
 
-export const mapParams = (event: React.FormEvent<HTMLFormElement>) => {
-		const inputValues = Array.from(event.currentTarget.elements)
+export const mapParams = (formData: Record<string, string>) => {
+		const params = new Set<string>()
 
-		const params = []
-		const errors = new Map()
-
-		for (const input of inputValues) {
-				const [ id, value, thisInputHaveValue ] = [ 'id', 'value', 'data-with-value' ]
-						.map(attr => input?.attributes?.getNamedItem(attr)?.value)
-
-				if (input?.id && value) {
-						if (!thisInputHaveValue) {
-								params.push(`${id}=${value}`)
-								continue
-						}
-
-						const [ splitId, splitValue ] = value.split('=')
-
-						if (!splitValue) {
-								errors.set(id, errorMessages.specification.requiredValue)
-								continue
-						}
-
-						params.push(`${id}=${splitId}=${splitValue}`)
+		for (const [ key, value ] of Object.entries(formData)) {
+				if (value) {
+						params.add(`${key}=${value}`)
 				}
 		}
-
-		if (errors.size) return errors
-		if (!params.length) return null
-
-		return params.join(',')
+		return params.size ? [ ...params ].join(',') : null
 }
