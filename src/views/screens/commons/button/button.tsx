daf9076caec9b1b3f0e71187b99ebb9bed77b1e1
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-30 04:08:25 -0300

    refactor(view): ensure common button separate container and text elements

--- src/views/screens/commons/button/button.tsx
+++ src/views/screens/commons/button/button.tsx
@@ -5,17 +5,21 @@ import { type ButtonHTMLAttributes } from 'react'
 
 export const Text = ({ children }: React.PropsWithChildren) => {
 		return (
-					<button className={'component-button text common'}>
-							{children}
-					</button>
+				<span className={'component-button text common'}>
+						{children}
+				</span>
 		)
 }
 
-export const Button = ({ children, ...rest }: React.PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>) => {
+export const Container = ({ children, ...rest }: React.PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>) => {
 		return (
-					<button {...rest} className={'component-button button common'}>
-							{children}
-					</button>
+				<button {...rest} className={'component-button button common'}>
+						{children}
+				</button>
 		)
 }
+
+export const Button = () => null
+
+Button.Container = Container
 Button.Text = Text
