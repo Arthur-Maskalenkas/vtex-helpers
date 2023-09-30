360bb253a74b26c9ad1b022ca66598399a4915fe
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-09-30 05:52:21 -0300

    test(view): ensure Header render a module title

--- tests/views/screens/searchProduct/components/header.spec.tsx
+++ tests/views/screens/searchProduct/components/header.spec.tsx
@@ -1,15 +1,31 @@
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it, SpyInstance, vi } from "vitest";
 import { Header } from "../../../../../src/views/screens/searchProduct/components/header.tsx";
 import { render } from "../../../../utils/test-utils.tsx";
+import * as SearchProductContextModule from "../../../../../src/views/screens/searchProduct/context/index.tsx";
 
 
 
 describe(`${Header.name} Tests Suite`, () => {
+		let searchProductContextModule: SpyInstance<any, any>
 
-		it('Should render the component', () => {
-				const { getByRole } = render(<Header/>);
+		beforeEach(() => {
+				searchProductContextModule = vi.spyOn(SearchProductContextModule, 'useSearchProductContext')
+		})
+
+		it('Should render correctly', () => {
+				searchProductContextModule.mockImplementation(() => {
+						return {
+								states: {
+										currentModule: 'product'
+								} as any,
+								dispatch: vi.fn()
+						}
+				})
+
+				const { getByRole } = render(<Header/>)
 
 				expect(getByRole('button', { name: 'Voltar' })).toBeInTheDocument()
+				expect(getByRole('heading', { name: 'product' })).toBeInTheDocument()
 		})
 
 })
