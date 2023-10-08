da7b3a63227ad61c7c83894d42bed8e6527d71e0
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-10-08 14:17:45 -0300

    refactor(data): allow null return on ProtocolDomLoadRuntime

--- src/services/data/protocols/dom/domLoadRuntime.ts
+++ src/services/data/protocols/dom/domLoadRuntime.ts
@@ -1,12 +1,8 @@
-import { InternalModelRuntime } from "../../../domain/internal/models/runtime.ts";
-
-
-
 export interface ProtocolDomLoadRuntime {
 		getByDom: () => ProtocolDomLoadRuntime.Result
 }
 
 export namespace ProtocolDomLoadRuntime {
 
-		export type Result = Promise<InternalModelRuntime>
+		export type Result = Promise<Record<string, any> | null>
 }
