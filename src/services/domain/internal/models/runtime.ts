7ae4fb11a93db90986e2f9d8a109c067d6aa1c89
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-10-08 12:26:49 -0300

    feat(domain): add class InternalModelRuntime

---
+++ src/services/domain/internal/models/runtime.ts
@@ -0,0 +1,126 @@
+export class InternalModelRuntime {
+		public readonly culture: ModelRuntime.Culture
+		public readonly pages: Record<string, ModelRuntime.Pages>
+		public readonly accountId: string
+		public readonly workspace: string
+		public readonly account: string
+		public readonly publicEndpoint: 'publicEndpoint' | null
+		public readonly query: { data: string }
+		public readonly serverQuery: { data: string }
+		public readonly binding: ModelRuntime.Binding
+
+		constructor({
+																culture,
+																pages,
+																accountId,
+																workspace,
+																account,
+																publicEndpoint,
+																query,
+																serverQuery,
+																binding
+														}: Record<string, any>) {
+				this.culture = culture
+				this.pages = pages
+				this.accountId = accountId
+				this.workspace = workspace
+				this.account = account
+				this.publicEndpoint = publicEndpoint
+				this.query = query
+				this.serverQuery = serverQuery
+				this.binding = binding
+		}
+
+}
+
+namespace Extension {
+		export type Block = {
+				blockId: string
+				extensionPointId: string
+				children: boolean
+				blockRole: string
+		}
+
+		export type Props = Record<string, any>
+}
+
+namespace Route {
+		export type PageContext = {
+				id: string
+				type: string
+		}
+
+		export type Params = {
+				id: string
+				department: string
+				category: string
+				subcategory: string
+		}
+
+		export type QueryString = {
+				map: string
+		}
+}
+
+export namespace ModelRuntime {
+		export type Pages = {
+				allowConditions: boolean
+				context: string
+				declarer: string
+				path: string
+				routeId: string
+				blockId: string
+				canonical: string
+				map: string[]
+		}
+
+		export type Binding = {
+				"id": string,
+				"canonicalBaseAddress": string
+		}
+
+		export type Route = {
+				domain: string
+				id: string
+				pageContext: Route.PageContext
+				params: Route.Params
+				path: string
+				pathId: string
+				queryString: Route.QueryString
+				rootName: string
+				ssr: boolean
+				blockId: string
+				canonicalPath: string
+				metaTags: any
+				routeId: string
+				title: any
+				varyContentById: boolean
+		}
+		export type Culture = {
+				availableLocales: any[]
+				country: string
+				currency: string
+				language: string
+				locale: string
+				customCurrencyDecimalDigits: any
+				customCurrencySymbol: string
+		}
+
+		export type Extension = {
+				before: string[]
+				around: string[]
+				after: string[]
+				blockId: string
+				blocks: Extension.Block[]
+				component: string
+				composition: string
+				hasContentSchema: boolean
+				hydration: string
+				implements: string[]
+				props: Extension.Props
+				render: string
+				track: any[]
+				content: Extension.Props
+				contentIds: string[]
+		}
+}
