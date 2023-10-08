852a4957a7a57b59171ddb41dedb24b1615d6322
Arthur Maskalenaks <maskalenkasdoc@gmail.com>
2023-10-08 13:30:52 -0300

    feat(data): ensure UseCaseLoadRuntime work correctly

--- src/services/data/useCases/dom/loadRuntime.ts
+++ src/services/data/useCases/dom/loadRuntime.ts
@@ -5,12 +5,13 @@ import { ProtocolDomLoadRuntime } from "../../protocols/dom/domLoadRuntime.ts";
 
 export class UseCaseLoadRuntime implements IUseCaseLoadRuntime {
 		constructor(
-				readonly domLoadRuntime: ProtocolDomLoadRuntime
+				private readonly domLoadRuntime: ProtocolDomLoadRuntime
 		) {
 		}
 
 		async load(): IUseCaseLoadRuntime.Result {
-				return null as any
+				const runtime = await this.domLoadRuntime.getByDom()
+				return runtime
 		}
 
 
